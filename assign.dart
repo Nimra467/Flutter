95bff89546c3c12ca6437ddab3a6b7c5aec23104
Nimra467 <57403206+Nimra467@users.noreply.github.com>
2024-03-31 00:34:16 +0500

    Update assign.dart

--- assign.dart
+++ assign.dart
@@ -1,10 +1,1 @@
-void main(){
-   Map information = {
-    "name": "Nimra",
-    "phone": "03000000",
-    "course": "mobile app dev",
-    "qualification": "bscs"
-  };
- var shortKeys = information.keys.where((key) => key.length == 4);
-  print(shortKeys);
-}
+
